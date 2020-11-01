15cf75c8bf4eeb4c9b3f899c0f11f2cb89510069
zombieteer <aryan.nigam1996@gmail.com>
2020-11-01 12:18:12 +0530

    backend user login and register complete

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,16 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  // if user details has been modified and password is not modified
+  if (!this.isModified("password")) {
+    next();
+  }
+  // else
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
